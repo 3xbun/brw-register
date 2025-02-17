e5f2780f07b5eec8aa55ad0b304436dfc0c7869d
Bunnasorn Kaewsiri <bunnasorn.k@outlook.com>
2025-02-17 19:18:10 +0700

    Create footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer>
+    Ready2Play Companion v{{ version }}
+  </footer>
+</template>
+
+<script setup>
+import { version } from '../../package.json';
+</script>
+
+<style scoped>
+footer {
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  text-align: center;
+  padding: .5em 0;
+  background-color: var(--primary-base-bg);
+}
+</style>
