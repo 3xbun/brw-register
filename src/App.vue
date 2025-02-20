1cab459dbd180b7a4ad6899cf34a532e9b60d9db
Bunnasorn Kaewsiri <bunnasorn.k@outlook.com>
2025-02-20 13:56:13 +0700

    Using localStorage to cache

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,42 @@
 <template>
   <div class="container">
-    <RouterView />
+    <Loading v-if="Users.length < 1" />
+    <RouterView v-else />
   </div>
   <Footer />
 </template>
 
 <script setup>
 import Footer from './components/Footer.vue';
+import axios from 'axios';
+import { onMounted, provide, ref } from 'vue';
+import Loading from './components/Loading.vue';
 
+const Users = ref([])
+const Equipments = ref([])
+
+provide('Users', Users)
+provide('Equipments', Equipments)
+
+onMounted(() => {
+  if (localStorage.getItem("UsersDB")) {
+    Users.value = JSON.parse(localStorage.getItem("UsersDB"))
+  }
+
+  if (localStorage.getItem("Equipments")) {
+    Users.value = JSON.parse(localStorage.getItem("Equipments"))
+  }
+
+  axios.get("https://n8n.3xbun.com/webhook/brw-api/users").then(res => {
+    Users.value = res.data
+    localStorage.setItem("UsersDB", JSON.stringify(res.data))
+  })
+
+  axios.get("https://n8n.3xbun.com/webhook/brw-api/wne").then(res => {
+    Equipments.value = res.data
+    localStorage.setItem("Equipments", JSON.stringify(res.data))
+  })
+})
 </script>
 
 <style scoped>
